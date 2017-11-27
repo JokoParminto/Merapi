64dd0f8992f69332ae5260cef0e846b901ec4317
rayzalzero <rayzalzero@gmail.com>
2017-11-27 11:21:49 +0700

    update error js

--- m2/application/views/home.php
+++ m2/application/views/home.php
@@ -525,7 +525,6 @@ antavaya tour travel  ">
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo site_url('resources/aio/js/jquery.1.11.1.js');?>"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript" src="<?php echo site_url('resources/aio/js/bootstrap.js');?>"></script> 
     <script type="text/javascript" src="<?php echo site_url('resources/aio/js/SmoothScroll.js');?>"></script> 
