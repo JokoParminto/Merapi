3286433b9e946bf8be1adac7e9807b892e83aa4a
rayzalzero <rayzalzero@gmail.com>
2017-11-27 17:38:29 +0700

    update_cantik

--- m2/application/views/layouts/main.php
+++ m2/application/views/layouts/main.php
@@ -51,7 +51,7 @@
                             <li class="dropdown user user-menu">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                     <img src="<?php echo site_url('resources/img/user2-160x160.jpg');?>" class="user-image" alt="User Image">
-                                    <span class="hidden-xs"><?php echo $this->session->userdata['logged_in'];?></span>
+                                    <span class="hidden-xs">Admin</span>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <!-- User image -->
